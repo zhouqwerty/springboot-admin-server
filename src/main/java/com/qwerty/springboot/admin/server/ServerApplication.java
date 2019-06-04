ddbc0f7f5d9b30ff454ea43a85c873b05c58311d
zhou <2891128968@qq.com>
2019-06-04 20:28:22 +0800

    Initial commit

---
+++ src/main/java/com/qwerty/springboot/admin/server/ServerApplication.java
@@ -0,0 +1,20 @@
+package com.qwerty.springboot.admin.server;
+
+
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@EnableAutoConfiguration
+@EnableAdminServer
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
